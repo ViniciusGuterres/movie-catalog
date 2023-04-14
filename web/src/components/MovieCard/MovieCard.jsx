ad1a87dc7b6f72e3e5f26069d3b21df57ec078a0
vinicius guterres <vinicius@spacecom.com.br>
2023-04-14 20:01:14 -0300

    Adding movie card component

---
+++ web/src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import './MovieCard.css';
+
+export default function MovieCard({ imgEndPoint, name, description, voteAverage }) {
+    const buildCardInformation = () => {
+        let descriptionSliced = description;
+
+        if (description.length > 233) {
+            descriptionSliced = `${description.slice(0, 233)}...`;
+        }
+
+        return (
+            <div className="movie-card-container">
+                <div className="movie-card-sub-container">
+                    <div className="movie-name-and-vote-container">
+                        <span className="movie-title">
+                            {name}
+                        </span>
+
+                        <strong className="movie-vote">{voteAverage}</strong>
+                    </div>
+
+                    <p>
+                        {descriptionSliced}
+                    </p>
+
+                    <button>
+                        Detalhes
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
+    const clickAtMovieImg = () => {
+        const movieTrailerUrl = `https://www.youtube.com/results?search_query=${name}trailer}`
+
+        window.open(movieTrailerUrl);
+    }
+
+    return (
+        <div>
+            <img
+                src={`https://image.tmdb.org/t/p/w300/${imgEndPoint}`}
+                onClick={() => clickAtMovieImg()}
+            />
+
+            {buildCardInformation()}
+        </div>
+    )
+}
