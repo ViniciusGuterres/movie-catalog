aa4d1adf639f529a755f6867ef572319ed95b1cf
vinicius guterres <vinicius@spacecom.com.br>
2023-04-27 22:25:10 -0300

    Adding save forms functions

--- web/src/views/Register/Register.jsx
+++ web/src/views/Register/Register.jsx
@@ -3,8 +3,61 @@ import Header from '../../components/Header/Header.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 
 import '../Register/Register.css';
+import { useState } from 'react';
 
-export default function Register() {
+export default function Register() {    
+    const [name, setName] = useState('');
+    const [tel, setTel] = useState('');
+    const [address, setAddress] = useState('');
+
+    // credit card states
+    const [creditCardCVCValue, setCreditCardCVCValue] = useState('');
+    const [creditCardNumberValue, setCreditCardNumberValue] = useState('');
+    const [creditCardNameValue, setCreditCardNameValue] = useState('');
+
+    // On change functions
+    const handleCreditCardCVCValueChange = event => {
+        const CVCValue = event.target.value;
+
+        setCreditCardCVCValue(CVCValue);
+    }
+
+    const handleCreditCardNumberValueChange = event => {
+        const numberValue = event.target.value;
+
+        setCreditCardNumberValue(numberValue);
+    }
+
+    const handleCreditCardNameValueChange = event => {
+        const nameValue = event.target.value;
+
+        setCreditCardNameValue(nameValue);
+    }
+
+    const handleSaveUser = () => {
+        const userReturnObj = {
+            name,
+            tel,
+            address,
+            creditCardCVCValue,
+            creditCardNameValue,
+        };
+
+        console.log('User: ', userReturnObj);
+        
+        cleanForms();
+        alert('Usuário cadastrado com sucesso');
+    }
+
+    const cleanForms = () => {
+        setName('');
+        setTel('');
+        setAddress('');
+        setCreditCardCVCValue('');
+        setCreditCardNameValue('');
+        setCreditCardNumberValue('');
+    }
+
     return (
         <>
             <Header
@@ -20,16 +73,22 @@ export default function Register() {
                         <input
                             placeholder='Nome'
                             type='text'
+                            value={name}
+                            onChange={event => setName(event.target.value)}
                         />
 
                         <input
                             placeholder='Telefone'
                             type='number'
+                            value={tel}
+                            onChange={event => setTel(event.target.value)}
                         />
 
                         <input
                             placeholder='Endereço'
                             type='text'
+                            value={address}
+                            onChange={event => setAddress(event.target.value)}
                         />
 
                         <div>
@@ -38,34 +97,44 @@ export default function Register() {
                     </div>
 
                     {/* Second form */}
-                    <div className='form'>
+                    <form className='form'>
                         <h1 className='form-title '>Dados pessoais</h1>
 
                         <input
                             placeholder='Nome do cartão'
                             type='text'
+                            onChange={handleCreditCardNameValueChange}
+                            value={creditCardNameValue}
                         />
 
                         <input
                             placeholder='Número do cartão'
                             type='number'
+                            max="20"
+                            maxLength = "20"
+                            value={creditCardNumberValue}
+                            onChange={handleCreditCardNumberValueChange}
                         />
 
                         <input
                             placeholder='CVC'
-                            type='number'
+                            type='password'
+                            max="3"
+                            maxLength = "3"
+                            value={creditCardCVCValue}
+                            onChange={handleCreditCardCVCValueChange}
                         />
 
                         <div>
 
                         </div>
-                    </div>
+                    </form>
                 </div>
 
                 <div>
                     <button
                         className='test'
-                        onClick={() => { }}
+                        onClick={handleSaveUser}
                     >
                         Assinar
                     </button>
